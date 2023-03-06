8f290fae8c646a683c9a55d98063906c334c7b3f
HwangUiYeon <121009009+HwangUiYeon@users.noreply.github.com>
2023-03-06 09:38:21 +0900

    0306
    character move

---
+++ Source/EldenRingVR/Private/PCPlayer.cpp
@@ -0,0 +1,92 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PCPlayer.h"
+#include "EnhancedInputSubsystems.h"
+#include <GameFramework/SpringArmComponent.h>
+#include <Camera/CameraComponent.h>
+#include "VRGameModeBase.h"
+#include "Engine/LocalPlayer.h"
+#include "EnhancedInputComponent.h"
+#include <DrawDebugHelpers.h>
+#include <Components/CapsuleComponent.h>
+
+
+// Sets default values
+APCPlayer::APCPlayer()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	// 스프링암, 카메라 컴포넌트
+	springArmComp = CreateDefaultSubobject<USpringArmComponent>(TEXT("springArmComp"));
+	cameraComp = CreateDefaultSubobject<UCameraComponent>(TEXT("cameraComp"));
+
+	// 스프링암을 루트에 붙임
+	springArmComp->SetupAttachment(RootComponent);
+	// 카메라를 스프링암에 붙임
+	cameraComp->SetupAttachment(springArmComp);
+	// 스프링암 위치,거리
+	springArmComp->SetRelativeLocation(FVector(0, 0, 0));
+	springArmComp->TargetArmLength = 400;
+	// 카메라 위치,각도
+	cameraComp->SetRelativeLocation(FVector(41, 0, 87));
+	cameraComp->SetRelativeRotation(FRotator(-20, 0, 0));
+	//입력값을 회전에 반영가능
+	bUseControllerRotationYaw = true;
+	springArmComp->bUsePawnControlRotation = true;
+	cameraComp->bUsePawnControlRotation = true;
+	
+
+
+}
+
+// Called when the game starts or when spawned
+void APCPlayer::BeginPlay()
+{
+	Super::BeginPlay();
+	//Enhanced Input 사용처리
+	auto PC = Cast<APlayerController>(GetWorld()->GetFirstPlayerController());
+	if (PC)
+	{
+		auto localPlayer = PC->GetLocalPlayer();
+		auto subSystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(localPlayer);
+		if (subSystem)
+		{
+			subSystem->AddMappingContext(IMC_VRInput, 0);
+		}
+	}
+}
+
+// Called every frame
+void APCPlayer::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void APCPlayer::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+	auto InputSystem = CastChecked<UEnhancedInputComponent>(PlayerInputComponent);
+	if (InputSystem)
+	{
+		InputSystem->BindAction(IA_Move, ETriggerEvent::Triggered, this, &APCPlayer::Move);
+		InputSystem->BindAction(IA_Mouse, ETriggerEvent::Triggered, this, &APCPlayer::Turn);
+	}
+}
+
+void APCPlayer::Move(const FInputActionValue& Values)
+{
+	FVector2D Axis = Values.Get<FVector2D>();
+	AddMovementInput(GetActorForwardVector(), Axis.X);
+	AddMovementInput(GetActorRightVector(), Axis.Y);
+}
+
+void APCPlayer::Turn(const FInputActionValue& Values)
+{
+	FVector2D Axis = Values.Get<FVector2D>();
+	AddControllerYawInput(Axis.X);
+	AddControllerPitchInput(Axis.Y);
+}
